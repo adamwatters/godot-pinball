b95dcfc9e026299666ef24f572f5403abeea5d2b
Adam Watters <adamwatters11@gmail.com>
2024-04-11 19:33:28 -0400

    pinball

--- Godot_Project/game/prefabs/flipper.gd
+++ Godot_Project/game/prefabs/flipper.gd
@@ -1,16 +1,20 @@
-extends HingeJoint3D
+extends Node3D
 
-@onready var flipper = $flipper
+@onready var flipper_joint = $flipper_joint
+@onready var flipper_paddle = $flipper_paddle
 @export var reversed = false
 
 func flip():
 	if reversed:
-		flipper.apply_impulse(Vector3(0, 300, 0), Vector3(1,0,-0.5))
+		flipper_paddle.apply_impulse(Vector3(0, 100, 0), Vector3(1,0,-0.5))
 	else:
-		flipper.apply_impulse(Vector3(0, 300, 0), Vector3(-1,0,-0.5))
+		flipper_paddle.apply_impulse(Vector3(0, 100, 0), Vector3(-1,0,-0.5))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if reversed:
+		pass
+		#flipper_joint.motor_target_velocity = flipper_joint.motor_target_velocity * -1
 	pass # Replace with function body.
 
 
--- Godot_Project/game/prefabs/launcher.gd
+++ Godot_Project/game/prefabs/launcher.gd
@@ -1,5 +1,10 @@
 extends RigidBody3D
 
 func _on_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouseButton  and event.pressed:
-		self.apply_impulse(Vector3(0,150,0))
+	if event is InputEventMouseButton and event.pressed:
+		print("pressed")
+		self.apply_impulse(Vector3(0,-100,0))
+		#self.apply_impulse(Vector3(0,150,0))
+
+func _process(delta):
+	self.apply_force(Vector3(0,150,0))
